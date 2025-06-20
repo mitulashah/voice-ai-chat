cc9202a14556854881a7b2b985ab457b95c50d75
Mitul Shah <mitulshah@microsoft.com>
2025-06-20 09:48:53 -0500

    chore: remove tracked server/dist and add .gitignore to prevent future tracking

--- server/dist/middleware/errorHandler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = errorHandler;
-function errorHandler(err, req, res, next) {
-    console.error('Error:', err);
-    res.status(500).json({ error: err.message });
-}
--- server/dist/types/api.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// Shared API types for server
-Object.defineProperty(exports, "__esModule", { value: true });
